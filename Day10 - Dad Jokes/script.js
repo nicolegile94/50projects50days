cb790bfb542be21ce46615f91a9f138591482daa
nicolegile94 <nicolegile94@gmail.com>
2023-11-07 10:13:56 -0600

    updated day 10

--- Day10 - Dad Jokes/script.js
+++ Day10 - Dad Jokes/script.js
@@ -5,12 +5,15 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
-    fetch('https://icanhazdadjoke.com', {
+async function generateJoke() {
+    const config = {
         headers: {
-        'Accept' : 'application/json'
-        }
-    }).then(res => res.json()).then(data => {
-        jokeEl.innerHTML = data.joke
-    })
+            Accept : 'application/json'
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+ 
 }
